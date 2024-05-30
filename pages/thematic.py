de4c95a621d7e1c4f728c611a4150bbacba6b39a
Wei Lu <luwei.here@gmail.com>
2024-05-30 15:26:24 -0400

    Fix css styling key

--- pages/thematic.py
+++ pages/thematic.py
@@ -10,7 +10,7 @@ layout = html.Div(children=[
             dbc.Tabs(id='thematic-tabs', active_tab='tab-education', children=[
                 dbc.Tab(label='Education', tab_id='tab-education'),
                 dbc.Tab(label='Health', tab_id='tab-health'),
-            ], style={"margin-bottom": "2rem"}),
+            ], style={"marginBottom": "2rem"}),
             html.Div(id='thematic-spinner', children=[
                 dbc.Spinner(color="primary", spinner_style={
                     "width": "3rem", "height": "3rem"
